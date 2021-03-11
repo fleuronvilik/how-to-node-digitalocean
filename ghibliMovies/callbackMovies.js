6242ea3efcd09539be0f9a5ea19ba5f08aee97bc
Fleuron Gnadekpa <vilfleur@gmail.com>
2021-03-11 18:56:51 +0100

    Log the movies list in a file instead

--- ghibliMovies/callbackMovies.js
+++ ghibliMovies/callbackMovies.js
@@ -1,4 +1,5 @@
 const request = require('request');
+const fs = require('fs');
 
 request('https://ghibliapi.herokuapp.com/films', (error, response, body) => {
   if (error) {
@@ -13,8 +14,18 @@ request('https://ghibliapi.herokuapp.com/films', (error, response, body) => {
 
   console.log('Processing our list of movies');
   movies = JSON.parse(body);
+  let moviesList = '';
   movies.forEach(movie => {
-    console.log(`${movie['title']}, ${movie['release_date']}`);
+    moviesList += `${movie['title']}, ${movie['release_date']}\n`;
+  });
+
+  fs.writeFile('callbackMovies.csv', moviesList, (error) => {
+    if (error) {
+      console.error(`Could not save the Ghibli movies to a file: ${error}`);
+      return;
+    }
+
+    console.log('Saved our list of movies to callbackMovies.csv');
   });
 });
 
