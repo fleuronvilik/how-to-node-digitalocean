d28e561d5d04f05598bedd53e6dc75f0feda9d9b
Fleuron Gnadekpa <vilfleur@gmail.com>
2021-03-11 18:22:54 +0100

    Complete request with a callback...
    to log data (the movies) in the console

--- ghibliMovies/callbackMovies.js
+++ ghibliMovies/callbackMovies.js
@@ -1,4 +1,20 @@
 const request = require('request');
 
-request('https://ghibliapi.herokuapp.com/films');
+request('https://ghibliapi.herokuapp.com/films', (error, response, body) => {
+  if (error) {
+    console.error(`Could not send request to API: ${error.message}`);
+    return;
+  }
+
+  if (response.statusCode != 200) {
+    console.error(`Expected status code 200 but received ${response.statusCode}`);
+    return;
+  }
+
+  console.log('Processing our list of movies');
+  movies = JSON.parse(body);
+  movies.forEach(movie => {
+    console.log(`${movie['title']}, ${movie['release_date']}`);
+  });
+});
 
