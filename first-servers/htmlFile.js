7e491c87ef9bc8dab7c26e475d83cf463ca07fa5
Fleuron Gnadekpa <vilfleur@gmail.com>
2021-03-13 23:55:46 +0100

    Serve HTML from a file

---
+++ first-servers/htmlFile.js
@@ -0,0 +1,26 @@
+const http = require('http');
+const host = 'localhost';
+const port = 8000;
+
+const fs = require('fs').promises;
+
+const requestListener = function(req, res) {
+  fs.readFile(`${__dirname}/index.html`)
+    .then(markup => {
+      res.setHeader("Content-Type", "text/html");
+      res.writeHead(200);
+      res.end(markup);
+    })
+    .catch(err => {
+      res.writeHead(500);
+      res.end(err);
+      return;
+    });
+};
+
+const server = http.createServer(requestListener);
+
+server.listen(port, host, () => {
+  console.log(`Server is running on http://${host}:${port}`);
+});
+
