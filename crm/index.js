82d49acdffa77b50b0274b38951bf1e24b58f485
Fleuron Gnadekpa <vilfleur@gmail.com>
2024-11-20 09:05:39 +0000

    add db connection and bodyParser

--- crm/index.js
+++ crm/index.js
@@ -1,8 +1,20 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import bodyParser from 'body-parser'
 import { route } from './src/routes'
 
 const app = express()
 const PORT = 3000
+
+// mongoose connection
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost:27017/crmdb", {
+    useNewUrlParser: true
+})
+
+// bodyParser setup
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
 route(app)
 
